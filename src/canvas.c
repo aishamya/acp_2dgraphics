73eb2a1f192ad034cee0cfe2c52db05d042a9018
Aishamya <aishamyaugowda@gmail.com>
2026-06-04 22:06:19 +0530

    updated src

---
+++ src/canvas.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "canvas.h"
+ void Canvas_initialization(char canvas[ROWS][COLS]){
+    int i,j;
+    for(i=0;i<ROWS;i++){
+        for(j=0;i<COLS;j++){
+            canvas[i][j]=' ';
+    }
+ }
+}
+void Canvas_display(char canvas[ROWS][COLS]){
+    int i,j;
+    for(i=0;i<ROWS;i++){
+        for(j=0;i<COLS;j++){
+            printf("%c",canvas[i][j]);
+    }
+    printf("\n");
+ }
+}
