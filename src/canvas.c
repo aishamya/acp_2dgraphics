409275306220c871859461a1ab266a81def67836
Aishamya <aishamyaugowda@gmail.com>
2026-06-05 16:07:38 +0530

    recctified

--- src/canvas.c
+++ src/canvas.c
@@ -11,7 +11,7 @@
 void Canvas_display(char canvas[ROWS][COLS]){
     int i,j;
     for(i=0;i<ROWS;i++){
-        for(j=0;i<COLS;j++){
+        for(j=0;j<COLS;j++){
             printf("%c",canvas[i][j]);
     }
     printf("\n");
