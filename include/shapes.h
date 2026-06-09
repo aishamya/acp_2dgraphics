6d0acdff9ffc0e1c5cbd4bf89d9e1797fa7a7ee1
Aishamya <aishamyaugowda@gmail.com>
2026-06-09 20:42:53 +0530

    updated

--- include/shapes.h
+++ include/shapes.h
@@ -2,7 +2,7 @@
 #define CANVAS_SHAPES
 void line(char canvas[100][100],int x1,int y1,int x2,int y2);
 void rectangle (char canvas[100][100],int x1,int y1,int x2,int y2);
-void circle(char canvas[100][100],int x,int y,int rad);
+void circle(char canvas[100][100],int x1,int y1,int rad);
 void triangle(char canvas[100][100],int x1,int y1,int x2,int y2,int x3,int y3);
 
 #endif 
